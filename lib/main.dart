d54a466aff79ae1eacdbe9368a1331aba37c9cb2
Onur Talantimur <onurtalantimur@gmail.com>
2021-11-18 14:53:50 +0300

    mainn.dart refactored.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'ui/widgets/control_panel.dart';
+import 'package:shortly/ui/screens/main_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,39 +21,7 @@ class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            SvgPicture.asset(
-              "assets/images/logo.svg",
-              cacheColorFilter: true,
-            ),
-            SvgPicture.asset(
-              "assets/images/illustration.svg",
-              fit: BoxFit.cover,
-            ),
-            Text("Let's get started!",
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.deepPurple)),
-            // ignore: prefer_const_constructors
-            SizedBox(
-              height: 20,
-            ),
-            Text("Past your first link into\nthe field to shoten it",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 20,
-            ),
-            ControlPanel(),
-          ],
-        ),
-      ),
+      body: MainScreen(),
     );
   }
 }
