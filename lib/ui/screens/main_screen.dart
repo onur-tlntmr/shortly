58c9044aa78161bb5e6daa05502b958d3fe424a7
Onur Talantimur <onurtalantimur@gmail.com>
2021-11-19 12:14:26 +0300

    main screen optimized

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -2,43 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:shortly/ui/widgets/control_panel.dart';
 
-class MainScreen extends StatelessWidget{
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return  SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          SvgPicture.asset(
-            "assets/images/logo.svg",
-            cacheColorFilter: true,
-          ),
-          SvgPicture.asset(
-            "assets/images/illustration.svg",
-            fit: BoxFit.cover,
-          ),
-          Text("Let's get started!",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.deepPurple)),
-          // ignore: prefer_const_constructors
-          SizedBox(
-            height: 20,
-          ),
-          Text("Past your first link into\nthe field to shoten it",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          SizedBox(
-            height: 20,
-          ),
-          ControlPanel(),
-        ],
+    return Column(children: [
+      Expanded(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(top:25),
+          children: [
+            SvgPicture.asset(
+              "assets/images/logo.svg",
+              cacheColorFilter: true,
+            ),
+            SvgPicture.asset(
+              "assets/images/illustration.svg",
+              fit: BoxFit.scaleDown,
+            ),
+            Text("Let's get started!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: "RobotoMono",
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.deepPurple)),
+            Text("Past your first link into\nthe field to shoten it",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: "RobotoMono",
+                    fontSize: 20, fontWeight: FontWeight.bold)),
+          ],
+        ),
       ),
-    );
+      ControlPanel(),
+    ]);
   }
-
 }
